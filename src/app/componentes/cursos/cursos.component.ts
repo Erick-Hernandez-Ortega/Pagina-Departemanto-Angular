4e25db504245cdf408a48487fbfa86828c17b5a7
Erick <erickramon47@live.com.mx>
2022-09-02 21:42:54 -0500

    Se pone uso de base de datos en la parte de cursos

--- src/app/componentes/cursos/cursos.component.ts
+++ src/app/componentes/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-cursos',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+  items: Observable<any[]>;
 
-  constructor() { }
+  constructor(firestore : AngularFirestore) { 
+    this.items = firestore.collection('cursos').valueChanges();
+  }
 
   ngOnInit(): void {
   }
