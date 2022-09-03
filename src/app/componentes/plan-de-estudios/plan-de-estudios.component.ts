a4d064869c10634f3f8b4acefad83e28a0b4e5a8
Erick <erickramon47@live.com.mx>
2022-09-02 21:18:27 -0500

    Se agrega en la parte de plan de estudios la base de datos

--- src/app/componentes/plan-de-estudios/plan-de-estudios.component.ts
+++ src/app/componentes/plan-de-estudios/plan-de-estudios.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-plan-de-estudios',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plan-de-estudios.component.css']
 })
 export class PlanDeEstudiosComponent implements OnInit {
+  items: Observable<any[]>;
+  materias2: Observable<any[]>;
+  materias3: Observable<any[]>;
+  materias4: Observable<any[]>;
+  materias5: Observable<any[]>;
+  materias6: Observable<any[]>;
 
-  constructor() { }
+  constructor(firestore: AngularFirestore) { 
+    this.items = firestore.collection('materias').valueChanges();
+    this.materias2 = firestore.collection('materias basicas particular').valueChanges();
+    this.materias3 = firestore.collection('especializante').valueChanges();
+    this.materias4 = firestore.collection('ciberseguridad').valueChanges();
+    this.materias5 = firestore.collection('desarrollo de software').valueChanges();
+    this.materias6 = firestore.collection('inteligencia de datos').valueChanges();
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
